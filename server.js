cf8b8ef0686e8c49feeb7a2dcc9b8e66a75e89d3
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-05-29 14:05:46 -0400

    code updated to match model information

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const helpers = require('./util/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    expires: 7200000
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
